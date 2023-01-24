e9e11cc24e3163583f9e5bb574018d922d5f38b1
gratisfo72 <120601943+gratisfo72@users.noreply.github.com>
2023-01-24 17:42:53 +0200

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -23,20 +23,14 @@ function createCard (cardData){
   cardElement.querySelector('.place__title').textContent = cardData.name;
   cardElement.querySelector('.place__like-button').addEventListener('click', handleLikeButton);
   cardElement.querySelector('.place__delete-button').addEventListener('click', handleDeleteButton);
-  elementPlaceImage.addEventListener('click', () => {
-    photoPopupImage.src = cardData.link;
-    photoPopupImage.alt = cardData.name;
-    photoPopup.querySelector('.popup__signature').textContent = cardData.name;
-    openPopup(photoPopup);
-  });
   return cardElement;
 }
-
-initialLoad(initialCards);
 
 
 function addCard (cardData){
   placesElement.prepend(createCard(cardData));
 }
 
+
+initialLoad(initialCards);
 
